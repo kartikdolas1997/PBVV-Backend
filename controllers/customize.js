c11f9933538824cae08a443dc2b08b9969df42c9
kartik <kartikdolas1997@gmail.com>
2020-11-17 10:19:58 +0530

    yellow

--- controllers/customize.js
+++ controllers/customize.js
@@ -28,4 +28,33 @@ async function submit_candi(req, res) {
   res.send({success:true})
 }
 
-module.exports = { submit_candi };
+async function submit_candi_yellow(req, res) {
+  console.log('Yellow called');
+  console.log(req.body);
+const { G1, G2, G3, G4, B1, B2, B3, B4 } = req.body;
+let girls = [G1, G2, G3, G4];
+let boys = [B1, B2, B3, B4];
+let house = "yellow";
+
+let query =
+  // "INSERT INTO abc.new_table (house,gender_id,names) VALUES (?,?,?)";
+  "UPDATE abc.new_table SET names = ? WHERE id = ?;"
+girls.forEach((girl) => {
+  ID = girls.indexOf(girl)+13;
+  console.log('ID '+ID+'  '+'girl '+girl);
+  config.query(query, [girl,ID] , function (err, rows, fields) {
+    console.log({ err, rows, fields });
+  });
+});
+boys.forEach((boy) => {
+  ID = boys.indexOf(boy)+9;
+  console.log('ID '+ID+'  '+'boy '+boy);
+  config.query(query, [boy,ID], function (err, rows, fields) {
+    console.log({ err, rows, fields });
+  });
+});
+
+res.send({success:true})
+}
+
+module.exports = { submit_candi, submit_candi_yellow };
--- routes/customize.js
+++ routes/customize.js
@@ -2,4 +2,5 @@ const router = require("express").Router();
 const submitCandidates = require("../controllers/customize");
 
 router.post("/red", submitCandidates.submit_candi);
+router.post("/yellow", submitCandidates.submit_candi_yellow);
 module.exports = router;

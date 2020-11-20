f7cf7b662b34cb2bc5e3624657387ae3bedd3429
kartik <kartikdolas1997@gmail.com>
2020-11-20 12:31:17 +0530

    customze

--- controllers/customize.js
+++ controllers/customize.js
@@ -57,4 +57,60 @@ boys.forEach((boy) => {
 res.send({success:true})
 }
 
-module.exports = { submit_candi, submit_candi_yellow };
+async function submit_candi_green(req, res) {
+  console.log(req.body);
+const { G1, G2, G3, G4, B1, B2, B3, B4 } = req.body;
+let girls = [G1, G2, G3, G4];
+let boys = [B1, B2, B3, B4];
+let house = "green";
+
+let query =
+  // "INSERT INTO abc.new_table (house,gender_id,names) VALUES (?,?,?)";
+  "UPDATE abc.new_table SET names = ? WHERE id = ?;"
+girls.forEach((girl) => {
+  ID = girls.indexOf(girl)+29;
+  console.log('ID '+ID+'  '+'girl '+girl);
+  config.query(query, [girl,ID] , function (err, rows, fields) {
+    console.log({ err, rows, fields });
+  });
+});
+boys.forEach((boy) => {
+  ID = boys.indexOf(boy)+25;
+  console.log('ID '+ID+'  '+'boy '+boy);
+  config.query(query, [boy,ID], function (err, rows, fields) {
+    console.log({ err, rows, fields });
+  });
+});
+
+res.send({success:true})
+}
+
+async function submit_candi_blue(req, res) {
+  console.log(req.body);
+const { G1, G2, G3, G4, B1, B2, B3, B4 } = req.body;
+let girls = [G1, G2, G3, G4];
+let boys = [B1, B2, B3, B4];
+let house = "blue";
+
+let query =
+  // "INSERT INTO abc.new_table (house,gender_id,names) VALUES (?,?,?)";
+  "UPDATE abc.new_table SET names = ? WHERE id = ?;"
+girls.forEach((girl) => {
+  ID = girls.indexOf(girl)+21;
+  console.log('ID '+ID+'  '+'girl '+girl);
+  config.query(query, [girl,ID] , function (err, rows, fields) {
+    console.log({ err, rows, fields });
+  });
+});
+boys.forEach((boy) => {
+  ID = boys.indexOf(boy)+17;
+  console.log('ID '+ID+'  '+'boy '+boy);
+  config.query(query, [boy,ID], function (err, rows, fields) {
+    console.log({ err, rows, fields });
+  });
+});
+
+res.send({success:true})
+}
+
+module.exports = { submit_candi, submit_candi_yellow, submit_candi_green, submit_candi_blue };
--- routes/customize.js
+++ routes/customize.js
@@ -3,4 +3,7 @@ const submitCandidates = require("../controllers/customize");
 
 router.post("/red", submitCandidates.submit_candi);
 router.post("/yellow", submitCandidates.submit_candi_yellow);
+router.post("/green", submitCandidates.submit_candi_green);
+router.post("/blue", submitCandidates.submit_candi_blue);
+
 module.exports = router;

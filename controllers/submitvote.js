5a8cce98ed7d4b50e4b0619efb2a3ff25b2a5150
kartik <kartikdolas1997@gmail.com>
2020-11-26 16:28:42 +0530

    submit vote done

--- controllers/submitvote.js
+++ controllers/submitvote.js
@@ -5,13 +5,21 @@ async function submit_vote(req, res) {
   console.log(req.body);
   const { house, headboy, headgirl } = req.body;
   let query =
-    "UPDATE abc.new_table SET no_of_votes = no_of_votes + 1 WHERE house =? and gender_id = ? and names = ? and gender_id = ? and names = ?;";
-  config.query(query, [house, "male", headboy,house, "female", headgirl], function (err, rows, fields) {
+    "UPDATE abc.new_table SET no_of_votes = no_of_votes + 1 WHERE house =? and gender_id = ? and names = ?;";
+    let query2 =
+    "UPDATE abc.new_table SET no_of_votes = no_of_votes + 1 WHERE house =? and gender_id = ? and names = ?";
+  config.query(query,[house, "male", headboy],
+    function (err, rows, fields) {
+      console.log({ err, rows, fields });
+      // res.send(rows);
+    }
+  );
+  config.query(query2,[house, "female", headgirl],
+  function (err, rows, fields) {
     console.log({ err, rows, fields });
     res.send(rows);
-  });
-
-  
+  }
+);
 }
 
 module.exports = { submit_vote };

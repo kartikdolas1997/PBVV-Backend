df0edad950925201e93f1bd6bbcf97416c858e44
kartik <kartikdolas1997@gmail.com>
2020-11-27 17:05:35 +0530

    reset dne

--- routes/reset.js
+++ routes/reset.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const resultsCandidates = require("../controllers/reset");
 
-router.post("/abc", resultsCandidates.reset_count); 
-
-router.post('/', (req, res) => {
-    res.send('Please read documentation for the API. (results)')
-  })
+router.post("/abc", resultsCandidates.reset_count);
+router.post("/", (req, res) => {
+  res.send("Please read documentation for the API. (results)");
+});
 
 module.exports = router;

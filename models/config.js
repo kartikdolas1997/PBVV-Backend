79f4140d94560681b0192a25ffdc95464d1e1d6a
kartik <kartikdolas1997@gmail.com>
2021-02-28 14:16:21 +0530

    env vars final

--- models/config.js
+++ models/config.js
@@ -1,21 +1,29 @@
 const mysql = require("mysql");
 
 // Kartik credentials
-if(process.env.JAWSDB_URL){
-  console.log("connected to jaws db");
-  var config = mysql.createConnection(process.env.JAWSDB_URL);
+// if(process.env.JAWSDB_URL){
+//   console.log("connected to jaws db");
+//   var config = mysql.createConnection(process.env.JAWSDB_URL);
 
-} else {
-  console.log("connected to local sql db");
+// } else {
+//   console.log("connected to local sql db");
 
 
   var config = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "goodness",
-    database: "abc",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
   });
-}
+
+  // var config = mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "goodness",
+  //   database: "abc",
+  // });
+// }
 
 
 
